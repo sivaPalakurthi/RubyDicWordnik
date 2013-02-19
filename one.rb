c63ea3d376a7e1b1c28e4fc5934bb58d2272d324
Siva Palakurthi <siva123fu@gmail.com>
2013-02-19 07:40:06 +0530

    wordnik working in ubuntu

--- one.rb
+++ one.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'json'
 
+%w(rubygems wordnik).each {|lib| require lib}
+Wordnik.configure do |config|
+  config.api_key ='557bea420b5c3daf4d0030a71cc0817242db37d2bc14774a3'
+end
+
 class RubyDictionary
   attr_accessor :word
   def initialize(word = "default")
@@ -8,136 +13,11 @@ class RubyDictionary
   end
 
   def definition
-    #    return Wordnik.word.get_definitions(@word)
-    string = '[
-      {
-        "textProns": [
-
-        ],
-        "sourceDictionary": "ahd-legacy",
-        "exampleUses": [
-
-        ],
-        "relatedWords": [
-
-        ],
-        "labels": [
-
-        ],
-        "citations": [
-
-        ],
-        "word": "response",
-        "text": "The act of responding.",
-        "sequence": "0",
-        "score": 0.0,
-        "partOfSpeech": "noun",
-        "attributionText": "from The American Heritage Dictionary of the English Language, 4th Edition"
-      },
-      {
-        "textProns": [
-
-        ],
-        "sourceDictionary": "ahd-legacy",
-        "exampleUses": [
-
-        ],
-        "relatedWords": [
-
-        ],
-        "labels": [
-
-        ],
-        "citations": [
-
-        ],
-        "word": "response",
-        "text": "A reply or an answer.",
-        "sequence": "1",
-        "score": 0.0,
-        "partOfSpeech": "noun",
-        "attributionText": "from The American Heritage Dictionary of the English Language, 4th Edition"
-      },
-      {
-        "textProns": [
-
-        ],
-        "sourceDictionary": "ahd-legacy",
-        "exampleUses": [
-
-        ],
-        "relatedWords": [
-
-        ],
-        "labels": [
-
-        ],
-        "citations": [
-
-        ],
-        "word": "response",
-        "text": "A reaction, as that of an organism or a mechanism, to a specific stimulus.",
-        "sequence": "2",
-        "score": 0.0,
-        "partOfSpeech": "noun",
-        "attributionText": "from The American Heritage Dictionary of the English Language, 4th Edition"
-      },
-      {
-        "textProns": [
-
-        ],
-        "sourceDictionary": "ahd-legacy",
-        "exampleUses": [
-
-        ],
-        "relatedWords": [
-
-        ],
-        "labels": [
-
-        ],
-        "citations": [
-
-        ],
-        "word": "response",
-        "text": "Ecclesiastical   Something that is spoken or sung by a congregation or choir in answer to the officiating minister or priest.",
-        "sequence": "3",
-        "score": 0.0,
-        "partOfSpeech": "noun",
-        "attributionText": "from The American Heritage Dictionary of the English Language, 4th Edition"
-      },
-      {
-        "textProns": [
-
-        ],
-        "sourceDictionary": "ahd-legacy",
-        "exampleUses": [
-
-        ],
-        "relatedWords": [
-
-        ],
-        "labels": [
-
-        ],
-        "citations": [
-
-        ],
-        "word": "response",
-        "text": "A responsory.",
-        "sequence": "4",
-        "score": 0.0,
-        "partOfSpeech": "noun",
-        "attributionText": "from The American Heritage Dictionary of the English Language, 4th Edition"
-      }
-    ]'
-    parsed = JSON.parse(string)
+    parsed =  Wordnik.word.get_definitions(@word)
     parsed.each do |res|
       tempWord = res['text']
       puts "#{tempWord}"
     end
-
-    return "definition of #{word}"
   end
 
   def synonyms
@@ -168,68 +48,7 @@ class RubyDictionary
   end
 
   def wordNotFound
-
-    string = '{
-      "totalResults": 36,
-      "searchResults": [
-        {
-          "lexicality": 0.0,
-          "count": 114,
-          "word": "simpl"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 1089104,
-          "word": "simply"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 945721,
-          "word": "simple"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 69170,
-          "word": "simplicity"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 43488,
-          "word": "simpler"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 32648,
-          "word": "simplest"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 28044,
-          "word": "simplify"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 21911,
-          "word": "simplified"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 19295,
-          "word": "simplistic"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 8489,
-          "word": "simplifies"
-        },
-        {
-          "lexicality": 0.0,
-          "count": 8073,
-          "word": "simplifying"
-        }
-      ]
-    }'
-    parsed = JSON.parse(string)
+    parsed = Wordnik.words.search_words(:query => '*#{@word}*', :include_part_of_speech => 'verb', :min_length => 5, :max_length => 20)
     parsed['searchResults'].each do |res|
       count = res['count']
       tempWord = res['word']
@@ -237,10 +56,6 @@ class RubyDictionary
         puts "#{tempWord}"
       end
     end
-
-    return 'similar word'
-    #    return Wordnik.words.search_words(@word, :allowRegex => 'false', :caseSensitive => 'true', :skip => 0, :limit => 10)
-    # check if count !=0 print
   end
 
   def game
